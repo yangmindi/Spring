fb6748967a7a305f851d347eebfb53a19d804039
ymd <1946334058@qq.com>
2019-10-29 16:40:10 +0800

    TypeFilter

--- src/main/java/com/atguigu/config/MainConfig.java
+++ src/main/java/com/atguigu/config/MainConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.config;
 
 import com.atguigu.bean.Person;
+import com.atguigu.service.BookService;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Controller;
 @ComponentScans(
         value = {
                 @ComponentScan(value = "com.atguigu",includeFilters = {
-                        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Controller.class)
+//                        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Controller.class),
+//                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {BookService.class}),
+                        @ComponentScan.Filter(type = FilterType.CUSTOM,classes = {MyTypeFilter.class})
                 },useDefaultFilters = false)
         }
 )

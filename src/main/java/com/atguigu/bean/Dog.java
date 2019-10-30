f395ba6e87268e7193bfbaae33e91e4fd9ac1b18
ymd <1946334058@qq.com>
2019-10-30 10:36:46 +0800

    @PostCOnstruct & @PreDestory

---
+++ src/main/java/com/atguigu/bean/Dog.java
@@ -0,0 +1,25 @@
+package com.atguigu.bean;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class Dog {
+    public Dog() {
+        System.out.println("Dog...constructor...");
+    }
+
+    //对象创建并赋值之后调用
+    @PostConstruct
+    public void init(){
+        System.out.println("Dog...@PostConstruct...");
+    }
+
+    //容器移除对象之前
+    @PreDestroy
+    public void destory(){
+        System.out.println("Dog...@PreDestory");
+    }
+}

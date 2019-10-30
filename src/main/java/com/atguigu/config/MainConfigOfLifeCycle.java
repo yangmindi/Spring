63cc408ce5373a6e4a67bc8e3c74ec8db40df348
ymd <1946334058@qq.com>
2019-10-30 10:51:06 +0800

    BeanPostProcessor

--- src/main/java/com/atguigu/config/MainConfigOfLifeCycle.java
+++ src/main/java/com/atguigu/config/MainConfigOfLifeCycle.java
@@ -23,6 +23,15 @@ import org.springframework.context.annotation.Scope;
  *
  * 1）指定初始化和销毁方法：
  *      通过@Bean指定init-method和destory-method
+ * 2）通过让Bean实现InitializingBean（定义初始化逻辑）
+ *                DisposableBean（定义销毁逻辑）
+ * 3）可以使用JSR250
+ *      @PostConstruct：在bean创建完成并且属性复制完成；来执行初始化方法
+ *      @PreDestory：在容器销毁bean之前通知我们进行清理工作
+ * 4）BeanPostProcessor[interface]:bean的后置处理器：
+ *      在bean初始化前后进行一些处理工作；
+ *      postProcessBeforeInitialization:在初始化之前工作
+ *      postProcessAfterInitialization：在初始化之后工作
  */
 @ComponentScan("com.atguigu.bean")
 @Configuration
